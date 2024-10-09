916c6d7e17ada73cb005b55d42dcbf307c25833c
Paula <psteverlynck@correo.um.edu.uy>
2024-10-09 13:54:42 -0300

    billboard repository

--- src/main/java/com/proyecto/proyectostic/repository/BillboardRepository.java
+++ src/main/java/com/proyecto/proyectostic/repository/BillboardRepository.java
@@ -2,6 +2,9 @@ package com.proyecto.proyectostic.repository;
 
 import com.proyecto.proyectostic.model.Billboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BillboardRepository extends JpaRepository<Billboard, Integer> {
+    boolean existsByCinema_CinemaId(Integer cinemaId);
 }
